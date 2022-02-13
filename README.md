d8b45c89b40d4f7884c4ffc17aae480d01e99f3d
liuxsdev <1028330144@qq.com>
2022-02-13 16:25:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vercel-function-starter
+
+run `rollup -c`
---
+++ api/index.js
@@ -0,0 +1,9 @@
+function handler(request, response) {
+    response.status(200).json({
+        body: request.body,
+        query: request.query,
+        cookies: request.cookies,
+    });
+}
+
+export { handler as default };
