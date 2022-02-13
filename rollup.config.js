b5d558e20b2ea77e4dd146dcf87a2d220dc0d641
liuxsdev <1028330144@qq.com>
2022-02-13 16:43:36 +0800

    api not support index.js?

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 export default {
     input: "src/index.js",
     output: {
-        file: "api/index.js",
+        file: "api/map.js",
         format: "esm",
     },
     plugins: [nodeResolve()],
